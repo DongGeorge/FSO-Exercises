99961d65db1b94b4a6531acc0d4fed39031b31f5
dong.george03@gmail.com <gdong37@gatech.edu>
2026-01-30 14:54:11 -0500

    Complete 1.4 by converting parts info into list

--- Part1/part1/src/App.jsx
+++ Part1/part1/src/App.jsx
@@ -1,17 +1,19 @@
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   // Renders name of course
   const Header = (props) => {
@@ -30,27 +32,29 @@ const App = () => {
 
   // Renders the parts and their number of exercies
   const Content = (props) => {
+    let parts = props.parts
     return (
       <>
-        <Part name={part1.name} exercise={part1.exercises}/>
-        <Part name={part2.name} exercise={part2.exercises}/>
-        <Part name={part3.name} exercise={part3.exercises}/>
+        <Part name={parts[0].name} exercise={parts[0].exercises}/>
+        <Part name={parts[1].name} exercise={parts[1].exercises}/>
+        <Part name={parts[2].name} exercise={parts[2].exercises}/>
       </>
     )
   }
 
   // Renders total number of exercises
   const Total = (props) => {
+    let parts = props.parts
     return (
-      <p>Number of exercises {props.count}</p>
+      <p>Number of exercises {parts[0].exercises + parts[1].exercises + parts[2].exercises}</p>
     )
   }
 
   return (
     <div>
-      <Header course={course}/>
-      <Content />
-      <Total count={part1.exercises + part2.exercises + part3.exercises}/>
+      <Header course={course} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   )
 }
