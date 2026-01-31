6c1727bb8873b8cc8a99352c537acfe1c39fb982
dong.george03@gmail.com <gdong37@gatech.edu>
2026-01-31 18:36:49 -0500

    Complete 1.10 by refactoring each button into a custom component

--- Part1/unicafe/src/App.jsx
+++ Part1/unicafe/src/App.jsx
@@ -33,6 +33,8 @@ const Statistics = ({good, neutral, bad}) => {
   }
 }
 
+const Button = (props) => (<button onClick={props.onClick}>{props.text}</button>)
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
@@ -42,9 +44,9 @@ const App = () => {
   return (
     <div>
       <h1>give feedback</h1>
-      <button onClick={() => setGood(good + 1)}>good</button>
-      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
-      <button onClick={() => setBad(bad + 1)}>bad</button>
+      <Button onClick={() => setGood(good + 1)} text="good" />
+      <Button onClick={() => setNeutral(neutral + 1)} text="neutral" />
+      <Button onClick={() => setBad(bad + 1)} text="bad" />
 
       <h1>statistics</h1>
 
