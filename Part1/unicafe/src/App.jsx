ed132b02825d4cea1f5916fd706e7f03cd98ef81
dong.george03@gmail.com <gdong37@gatech.edu>
2026-01-31 18:22:52 -0500

    Complet 1.7 with added statistics

--- Part1/unicafe/src/App.jsx
+++ Part1/unicafe/src/App.jsx
@@ -5,6 +5,16 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const totalCount = good + neutral + bad
+
+  const [goodWeight, neutralWeight, badWeight] = [1, 0, -1]
+
+  const calculateAverage = () => {
+    let score = goodWeight * good + neutralWeight * neutral + badWeight * bad
+    return score / totalCount
+  }
+
+  const positivePercentage = () => good / totalCount
 
   return (
     <div>
@@ -17,6 +27,9 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {totalCount}</p>
+      <p>average {calculateAverage()}</p>
+      <p>positive {positivePercentage()} %</p>
     </div>
   )
 }
