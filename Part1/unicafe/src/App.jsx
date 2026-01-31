69e5b2e6c81412a0ee8d5ece311a1312e6be4b52
dong.george03@gmail.com <gdong37@gatech.edu>
2026-01-31 18:25:50 -0500

    Complete 1.8 by extracting additional statistics to custom component

--- Part1/unicafe/src/App.jsx
+++ Part1/unicafe/src/App.jsx
@@ -1,10 +1,6 @@
 import { useState } from 'react'
 
-const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+const Statistics = ({good, neutral, bad}) => {
   const totalCount = good + neutral + bad
 
   const [goodWeight, neutralWeight, badWeight] = [1, 0, -1]
@@ -17,6 +13,21 @@ const App = () => {
   const positivePercentage = () => good / totalCount
 
   return (
+    <>
+      <p>all {totalCount}</p>
+      <p>average {calculateAverage()}</p>
+      <p>positive {positivePercentage()} %</p>
+    </>
+  )
+}
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
     <div>
       <h1>give feedback</h1>
       <button onClick={() => setGood(good + 1)}>good</button>
@@ -27,9 +38,8 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
-      <p>all {totalCount}</p>
-      <p>average {calculateAverage()}</p>
-      <p>positive {positivePercentage()} %</p>
+      
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
