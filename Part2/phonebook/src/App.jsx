fc2fc0dd9731d0075e016fde35bdca0de25af142
dong.george03@gmail.com <gdong37@gatech.edu>
2026-02-01 21:49:33 -0500

    Refactor Form into separate component

--- Part2/phonebook/src/App.jsx
+++ Part2/phonebook/src/App.jsx
@@ -1,16 +1,10 @@
 import { useState } from 'react'
 
-const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
-  const [newName, setNewName] = useState('')
-  const [newNumber, setNumber] = useState('')
-  const [filter, setFilter] = useState('')
+const Filter = (props) => {
 
+}
+
+const PersonForm = ({persons, setPersons, newName, setNewName, newNumber, setNumber}) => {
   const nameIsNotPresent = (name) => persons.find(element => element.name === name) === undefined
 
   const handleSubmit = (event) => {
@@ -30,6 +24,36 @@ const App = () => {
     setNumber('')
   }
 
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        name: <input value={newName} onChange={(event) => setNewName(event.target.value)} />
+      </div>
+      <div>
+        number: <input value={newNumber} onChange={(event) => setNumber(event.target.value)} />
+      </div>
+      <div>
+        <button type="submit">add</button>
+      </div>
+    </form>
+  )
+}
+
+const Persons = (props) => {
+
+}
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNumber] = useState('')
+  const [filter, setFilter] = useState('')
+
   const filteredContacts = filter === ''
     ? persons
     : persons.filter(person => person.name.toLowerCase().startsWith(filter))
@@ -43,17 +67,11 @@ const App = () => {
           filter shown with <input value={filter} onChange={(event) => setFilter(event.target.value)}/>
         </div>
       <h2>add a new</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          name: <input value={newName} onChange={(event) => setNewName(event.target.value)} />
-        </div>
-        <div>
-          number: <input value={newNumber} onChange={(event) => setNumber(event.target.value)} />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
+      <PersonForm
+        persons={persons} setPersons={setPersons}
+        newName={newName} setNewName={setNewName}
+        newNumber={newNumber} setNumber={setNumber}
+      />
       <h2>Numbers</h2>
       {filteredContacts.map(person => <p key={person.name}>{person.name} {person.number}</p>)}
     </div>
