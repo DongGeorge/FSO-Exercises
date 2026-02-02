c6ed1a53611124a0975329756477b7c1555926fe
dong.george03@gmail.com <gdong37@gatech.edu>
2026-02-01 21:32:59 -0500

    Complete 2.8 by adding new number attribute to saved contacts

--- Part2/phonebook/src/App.jsx
+++ Part2/phonebook/src/App.jsx
@@ -2,40 +2,49 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { 
+      name: 'Arto Hellas',
+      number: '040-1234567'
+    }
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNumber] = useState('')
 
   const nameIsNotPresent = (name) => persons.find(element => element.name === name) === undefined
 
-  const handleNewName = (event) => {
+  const handleSubmit = (event) => {
     event.preventDefault()
     console.log(newName)
 
     if (nameIsNotPresent(newName)) {
       const newEntry = {
-        name: newName
+        name: newName,
+        number: newNumber
       }
       setPersons(persons.concat(newEntry))
     } else {
       alert(`${newName} is already added to phonebook`)
     }
     setNewName('')
+    setNumber('')
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={handleNewName}>
+      <form onSubmit={handleSubmit}>
         <div>
           name: <input value={newName} onChange={(event) => setNewName(event.target.value)} />
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={(event) => setNumber(event.target.value)} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => <p key={person.name}>{person.name}</p>)}
+      {persons.map(person => <p key={person.name}>{person.name} {person.number}</p>)}
     </div>
   )
 }
