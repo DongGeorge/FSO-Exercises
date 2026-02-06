599ac0ca891e23fe2f3f6e25b47a342bb70f8455
dong.george03@gmail.com <gdong37@gatech.edu>
2026-02-05 19:48:53 -0500

    Add ability to filter countries

--- Part2/data-for-countries/src/App.jsx
+++ Part2/data-for-countries/src/App.jsx
@@ -2,24 +2,35 @@ import axios from 'axios'
 import { useState, useEffect } from 'react'
 
 const App = () => {
+  const [allCountries, setCountries] = useState([])
   const [filter, setFilter] = useState('')
+  const [filteredCountries, setFilteredCountries] = useState([])
   const countryUrl = 'https://studies.cs.helsinki.fi/restcountries/api'
-  let allCountries = null
 
   useEffect(() => {
     axios.get(`${countryUrl}/all`)
       .then(response => {
         console.log(response)
-        allCountries = response.data
+        setCountries(response.data)
       })
       .catch(error => console.log('Communication with countries API failed'))
   }, [])
+
+  const filterCountries = () => {
+    return allCountries.filter(country => country.name.common.toLowerCase().includes(filter))
+  }
+
+  const handleFilter = event => {
+    setFilter(event.target.value)
+    setFilteredCountries(filterCountries())
+    // console.log(filteredCountries)
+  }
 
   return (
     <>
       <div>
         <p>find countries </p>
-        <input value={filter} onChange={event => setFilter(event.target.value)}/>
+        <input value={filter} onChange={handleFilter}/>
       </div>
     </>
   )
