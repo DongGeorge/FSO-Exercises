7ce8366a26815efcd3b6c99e3540e146cc3fc5ed
dong.george03@gmail.com <gdong37@gatech.edu>
2026-02-05 20:24:37 -0500

    Incorporate filter and choice between countries

--- Part2/data-for-countries/src/App.jsx
+++ Part2/data-for-countries/src/App.jsx
@@ -1,6 +1,47 @@
 import axios from 'axios'
 import { useState, useEffect } from 'react'
 
+const CountryItem = ({ setCountry, country }) => {
+  return (
+    <div>
+      <p style={{display: 'inline'}}>{country.name.common} </p>
+      <button onClick={() => setCountry(country)}>Show</button>
+    </div>
+  )
+}
+
+const CountryData = ({ filteredCountries }) => {
+  const [chosenCountry, setCountry] = useState(null)
+  // console.log(filteredCountries.length)
+
+  useEffect(() => {
+    if (filteredCountries.length === 1) {
+      setCountry(filteredCountries[0])
+    } else {
+      setCountry(null)
+    }
+  }, [filteredCountries])
+
+  if (filteredCountries.length > 10) {
+    return (
+      <p>Too many matches, specify another filter</p>
+    )
+  } else if (chosenCountry) {
+    // console.log(chosenCountry.name)
+    return (
+      <>
+        <div>hi {chosenCountry.name.common}</div>
+      </>
+    )
+  } else {
+    return (
+      <>
+        {filteredCountries.map(country => <CountryItem setCountry={setCountry} country={country} key={country.name.common} />)}
+      </>
+    )
+  }
+}
+
 const App = () => {
   const [allCountries, setCountries] = useState([])
   const [filter, setFilter] = useState('')
@@ -16,14 +57,15 @@ const App = () => {
       .catch(error => console.log('Communication with countries API failed'))
   }, [])
 
-  const filterCountries = () => {
-    return allCountries.filter(country => country.name.common.toLowerCase().includes(filter))
+  const filterCountries = (str) => {
+    // console.log(`filter: ${str}`)
+    setFilteredCountries(allCountries.filter(country => country.name.common.toLowerCase().includes(str)))
   }
 
   const handleFilter = event => {
-    setFilter(event.target.value)
-    setFilteredCountries(filterCountries())
-    // console.log(filteredCountries)
+    const newFilter = event.target.value
+    setFilter(newFilter)
+    filterCountries(newFilter)
   }
 
   return (
@@ -31,6 +73,7 @@ const App = () => {
       <div>
         <p>find countries </p>
         <input value={filter} onChange={handleFilter}/>
+        <CountryData filteredCountries={filteredCountries}/>
       </div>
     </>
   )
