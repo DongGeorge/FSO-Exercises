7de546be431cd1eaf7d9149ac20fcecde60b528e
DongGeorge <143650315+DongGeorge@users.noreply.github.com>
2026-06-12 12:45:42 -0400

    Dynamic port usage

--- Part3/PhonebookServer/index.js
+++ Part3/PhonebookServer/index.js
@@ -95,8 +95,7 @@ app.post('/api/persons', (req, res) => {
     res.status(200).send(newEntry)
 })
 
-
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT)
 
 console.log(`App started on port ${PORT}`)
